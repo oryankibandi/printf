3d3e5c20fbe310aa1d0d84fc49ac297ffe19baeb
MDoreen <dmachoni@gmail.com>
2022-04-20 08:15:43 -0700

    debugging

--- print_number.c
+++ print_number.c
@@ -1,32 +1,10 @@
 #include "main.h"
 
-/**
- * print_int - prints integer
- * @var:integer to be printed
- * Return: void
- *
- */
-int print_int(int var)
+int _puts(char *s)
 {
-
-	int i = 0;
-
-	if (var < 0)
-	{
-		_putchar('-');
-		i++;
-		var = var * -1;
-	}
-
-	if (var == 0)
-	_putchar('0');
-	i++;
-
-	if (var / 10)
-		print_int(var / 10);
-
-	_putchar(var % 10 + '0');
-	i++;
-
-	return (i);
+    while (*s != '\0')
+    {
+        _putchar(*s++);
+    }
+    return (0);
 }
