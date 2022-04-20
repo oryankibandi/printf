4deecf884533b61bade73d5ef74eadaf0ac0f883
MDoreen <dmachoni@gmail.com>
2022-04-20 08:28:34 -0700

    Header file

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _printf(char *format, ...);
 int _putchar(char c);
+int _puts(char *s);
 
 #endif
